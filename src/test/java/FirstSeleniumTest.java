169ee2d931b2d80896318b292a21d2660bb3dc06
T.Tatsenko <tatsenko.tetiana@gmail.com>
2025-06-23 13:03:43 +0200

    Added first test class

--- src/test/java/FirstSeleniumTest.java
+++ src/test/java/FirstSeleniumTest.java
@@ -1,9 +1,31 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class FirstSeleniumTest {
 
+    WebDriver driver;
     @BeforeMethod
     public void setUp() {
-
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.google.com/");
     }
+
+    @Test
+    public void openGoogleTest() {
+        System.out.println("Open");
+    }
+
+    @AfterMethod(enabled = true)
+    public void tearDown() {
+        driver.quit();
+    }
+
+
 }
